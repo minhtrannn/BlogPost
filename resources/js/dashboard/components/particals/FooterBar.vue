d910ed550b6017fbe567c09e6620e1eea689d6f5
minhtrannn <minhmom98@gmail.com>
2020-11-27 17:27:16 +0700

    first commit

---
+++ resources/js/dashboard/components/particals/FooterBar.vue
@@ -0,0 +1,7 @@
+<template>
+  <footer>
+    <div class="copy-right text-center">
+      <span>© Vietnam 2020. All rights reserved. </span><a class="item" href="http://www.miitbeian.gov.cn/">MinhTran Corporation</a>
+    </div>
+  </footer>
+</template>
